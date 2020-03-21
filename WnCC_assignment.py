e2b4384d01cb736ed0e462d4cbb9659c06fc593d
Liza23 <53112806+Liza23@users.noreply.github.com>
2020-03-21 23:41:52 +0530

    Update WnCC_assignment.py

--- WnCC_assignment.py
+++ WnCC_assignment.py
@@ -3,25 +3,27 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL="https://gadgets.ndtv.com/news" #url of the website used for we scrapping
+
+URL="https://gadgets.ndtv.com/news"	 #url of the website used for we scrapping
 r=requests.get(URL)
 #web scrapping is checked to be legalised on this website
 
+
 soup=BeautifulSoup(r.content,'html5lib')
-# We create a BeautifulSoup object by passing two arguments:
-# r.content (raw HTML content) & html5lib (specifying the HTML parser)
+# We create a BeautifulSoup object by passing two arguments
 
 tech_news=[]	#list for storing tech news
 table=soup.find('div',attrs={'class':'recent_news_widget'})
 #find() returns the first matching element through the argument passed
 
 for row in table.findAll('li',attrs={'data-tb-region-item'}):
-#tech news is stored in the list 'data-tb-region-item'
+	#tech news is stored in the list 'data-tb-region-item'
 	news={}
 	news['title']=row.text
 	tech_news.append(news.text)
 
-filename='tech_news.csv'    #create a csv file to store output
+filename='tech_news.csv'    	#create a csv file to store output
+
 with open(filename,'w') as file:
 	writer=csv.DictWriter(file,['title'])
 	writer.writeheader()
