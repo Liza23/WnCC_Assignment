48c0556a829c58458f8f34108ef85c986d917cae
Liza23 <53112806+Liza23@users.noreply.github.com>
2020-03-21 23:39:40 +0530

    Update WnCC_assignment.py

--- WnCC_assignment.py
+++ WnCC_assignment.py
@@ -1,25 +1,32 @@
+#these are the python libraries which are used to extract data out of HTML files
 import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL="https://gadgets.ndtv.com/news"
+URL="https://gadgets.ndtv.com/news" #url of the website used for we scrapping
 r=requests.get(URL)
+#web scrapping is checked to be legalised on this website
 
 soup=BeautifulSoup(r.content,'html5lib')
+# We create a BeautifulSoup object by passing two arguments:
+# r.content (raw HTML content) & html5lib (specifying the HTML parser)
 
-recent_news_widget=[]
+tech_news=[]	#list for storing tech news
 table=soup.find('div',attrs={'class':'recent_news_widget'})
+#find() returns the first matching element through the argument passed
 
-for row in table.findAll('div',attrs={'class':'recent_news_widget'}):
+for row in table.findAll('li',attrs={'data-tb-region-item'}):
+#tech news is stored in the list 'data-tb-region-item'
 	news={}
 	news['title']=row.text
-	recent_news_widget.append(news.text)
+	tech_news.append(news.text)
 
-filename='tech_news.csv'
+filename='tech_news.csv'    #create a csv file to store output
 with open(filename,'w') as file:
 	writer=csv.DictWriter(file,['title'])
 	writer.writeheader()
-	for news in recent_news_widget:
+	
+	for news in tech_news:
 		writer.writerow(news)
 
 
